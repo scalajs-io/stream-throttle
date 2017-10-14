9c2e2acb910ce613f3686cc83e84106d6926c333
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2017-10-14 12:09:40 -0700

    Release v0.4.2

--- src/main/scala/io/scalajs/npm/streamthrottle/ThrottleOptions.scala
+++ src/main/scala/io/scalajs/npm/streamthrottle/ThrottleOptions.scala
@@ -1,7 +1,7 @@
 package io.scalajs.npm.streamthrottle
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.ScalaJSDefined
+
 
 /**
   * Throttle Options
@@ -9,7 +9,7 @@ import scala.scalajs.js.annotation.ScalaJSDefined
   * @param chunksize (optional) is the maximum chunk size into which larger writes are decomposed;
   *                  the default is opts.rate/10.
   */
-@ScalaJSDefined
+
 class ThrottleOptions(val rate: Double,
                       val chunksize: js.UndefOr[Double] = js.undefined)
   extends js.Object
